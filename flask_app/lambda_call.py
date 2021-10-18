debd0d43731f2b4cea27aa29d580ea22db5087f0
Matt Paterson <hello@hiremattpaterson.com>
2021-10-17 20:38:36 -0700

    sending lambda code in lambda_call.py

--- flask_app/lambda_call.py
+++ flask_app/lambda_call.py
@@ -1,10 +1,10 @@
 import json
 
 def lambda_handler(event, context):
-
-    #call: https://pok2uapqb2.execute-api.us-west-2.amazonaws.com/users_api_dev/myresource?user_handle=42
-
+    import pandas as pd
     import boto3
+    import io
+
     #first_name = event['queryStringParameters']['first_name']
     #last_name = event['queryStringParameters']['last_name']
 
@@ -15,10 +15,24 @@ def lambda_handler(event, context):
 
 
     app_response = {}
-    #app_response['message'] = 'hello these are the 10 most similar users to user_handle: ', user_handle
     app_response['message'] = 'hello these are the 10 most similar users to user_handle: ' + user_handle
-    app_response['user_handle'] = user_handle
-    app_response['age'] = "10 closest listed out"
+
+
+
+    # Get the lookup table data and display the results
+    bucket_name = 'mp-pluralsight-demo-20211016'
+    filename = 'data/sims_df.csv'
+
+    s3 = boto3.client('s3')
+    obj = s3.get_object(Bucket=bucket_name, Key=filename)
+    sims_df = pd.read_csv(io.BytesIO(obj['Body'].read()), index_col='user_handle')
+
+    search = 42
+    print("*"*30)
+    #print("You entered user handle:", search)
+    print("The closest 10 user handles and their cosine similarities are:")
+    print(sims_df.loc[search].sort_values(ascending=False)[1:11])
+    print("*"*30)
 
     responseObject = {}
     responseObject['statusCode']=200
